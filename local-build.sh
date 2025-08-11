2d342dcc60bbb440ef95f7ae0762420028fdeb4f
Michael Limiero <michael.limiero@datawalk.com>
2025-08-11 14:46:06 -0400

    wip

---
+++ local-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eEuo pipefail
+
+if [[ ! -d .venv ]]
+then
+	uv venv .venv
+fi
+
+source .venv/bin/activate
+uv pip install west
+
+if [[ $# -lt 2 ]]; then
+	echo "Usage: $0 <SHIELD> <BOARD>" >&2
+	exit 1
+fi
+
+SHIELD="$1"
+BOARD="$2"
+shift 2
+EXTRA_ARGS=($@)
+
+TMPDIR="$(mktemp -d)"
+# trap "rm -rf $TMPDIR" EXIT
+mkdir "$TMPDIR/zephyr"
+ln -s "$PWD/zephyr/module.yml" "$TMPDIR/zephyr"
+ln -s "$PWD/boards" "$PWD/config" "$PWD/ardux.dtsi" "$TMPDIR"
+ls -l "$TMPDIR"
+
+west build -s zmk/app -d "build/${SHIELD}_${BOARD}" -b "${BOARD}" -S studio-rpc-usb-uart -- -DZMK_CONFIG="${PWD}/config" -DSHIELD="${SHIELD}" -DZMK_EXTRA_MODULES="$TMPDIR" -DCONFIG_ZMK_STUDIO=y "${EXTRA_ARGS[@]}"
